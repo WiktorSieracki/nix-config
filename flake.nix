0039776dc1d267df610a6f6b3d0e0d3b72472303
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2024-12-13 18:11:05 +0000

    fix variables

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
       username = builtins.getEnv "USER";
     in {
-      homeConfigurations."${username}" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
