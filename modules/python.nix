a0cf9e87c1b2cf1c6c83fbbbe77349cb946bdb18
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-01-07 22:11:46 +0100

    Add additional Python packages to home configuration

--- modules/python.nix
+++ modules/python.nix
@@ -4,6 +4,11 @@
   home.packages = [
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.numpy
+      python-pkgs.pandas
+      python-pkgs.matplotlib
+      python-pkgs.scipy
+      python-pkgs.opencv4
+      python-pkgs.scikit-image
     ]))
   ];
 }
