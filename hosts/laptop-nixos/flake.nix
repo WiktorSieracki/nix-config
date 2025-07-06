df1b1230dd16ddb1d297028429c89a79e425d212
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-07-06 21:18:19 +0200

    Refactor flake.nix: update description and restructure outputs for Home Manager configuration

--- hosts/laptop-nixos/flake.nix
+++ hosts/laptop-nixos/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nixos config flake";
+  description = "Home Manager configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -11,16 +11,41 @@
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
+    { nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
     {
-      # use "nixos", or your hostname as the name of the configuration
-      # it's a better practice than "default" shown in the video
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
+      nixosConfigurations = {
+        laptop-nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.wiktor = import ./home.nix;
+              home-manager.extraSpecialArgs = {
+                hostname = "laptop-nixos";
+                flakeRoot = ./../..;
+              };
+            }
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        "wiktor@laptop-nixos" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+          extraSpecialArgs = {
+            hostname = "laptop-nixos";
+            flakeRoot = ./../..;
+          };
+        };
       };
     };
 }
