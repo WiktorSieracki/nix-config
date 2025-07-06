cbcc530ec58a5779ba6976d5dab8f58bd4849bf8
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-07-07 01:32:01 +0200

    Fix Firefox module: update extensions configuration and add settings to prevent auto-disable

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -6,13 +6,16 @@
 
     profiles.default = {
       # Firefox extensions
-      extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
+      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
         bitwarden
         ublock-origin
         sponsorblock
         vimium-c
         darkreader
       ];
+      settings = {
+        "extensions.autoDisableScopes" = 0; # Disable auto-disabling of extensions
+      };
 
     };
   };
