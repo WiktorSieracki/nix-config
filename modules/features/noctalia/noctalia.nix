fcb04da0f7b33f692851d8f27bb5e60bf4ab39ed
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-03-23 18:35:37 +0100

    feat: add Noctalia configuration files for enhanced settings management

--- modules/features/noctalia/noctalia.nix
+++ modules/features/noctalia/noctalia.nix
@@ -6,9 +6,9 @@
   perSystem = {pkgs, ...}: {
     packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
-      # settings =
-      #   (builtins.fromJSON
-      #     (builtins.readFile ./noctalia.json)).settings;
+      settings =
+        (builtins.fromJSON
+          (builtins.readFile ./noctalia.json)).settings;
     };
   };
 }
