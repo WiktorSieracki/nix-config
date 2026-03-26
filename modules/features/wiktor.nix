b36f9b5578bc32458fcd082ccd6afb79d30d46ec
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-03-26 13:19:43 +0100

    feat: restructure wiktor.nix to include backup file extension and organize modules

--- modules/features/wiktor.nix
+++ modules/features/wiktor.nix
@@ -1,7 +1,22 @@
-{self, ...}: {
-  flake.modules.homeManager.wiktor = {pkgs, ...}: {
-    home.username = "wiktor";
-    home.homeDirectory = "/home/wiktor";
-    home.stateVersion = "24.11";
+{ self, ... }:
+{
+  flake.modules = {
+
+    nixos.wiktor =
+      { pkgs, ... }:
+      {
+        home-manager.backupFileExtension = ".bak";
+      };
+
+    homeManager.wiktor =
+      { pkgs, ... }:
+      {
+        home.username = "wiktor";
+        home.homeDirectory = "/home/wiktor";
+        home.stateVersion = "24.11";
+
+        programs.home-manager.enable = true;
+
+      };
   };
 }
