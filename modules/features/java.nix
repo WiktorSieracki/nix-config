36e63de8c2c7ac5d405e80fc88b8843b0cd17438
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-03-27 12:53:29 +0100

    refactor: simplify java module configuration in java.nix

--- modules/features/java.nix
+++ modules/features/java.nix
@@ -1,34 +1,16 @@
 {
-  flake.modules = {
-    nixos.java = {pkgs, ...}: {
-      environment.sessionVariables = {
-        JAVA_HOME = "${pkgs.zulu25}";
-      };
-
-      programs.java = {
-        enable = true;
-        package = pkgs.zulu25;
-      };
-
-      environment.systemPackages = with pkgs; [
-        gradle_9
-      ];
+  flake.modules.nixos.java = {pkgs, ...}: {
+    environment.sessionVariables = {
+      JAVA_HOME = "${pkgs.zulu25}";
     };
 
-    # homeManager.java = {pkgs, ...}: {
-    #   programs.fish.enable = true;
-    #   programs.fish.shellInit = ''
-    #     set -gx JAVA_HOME ${pkgs.zulu25}
-    #   '';
+    programs.java = {
+      enable = true;
+      package = pkgs.zulu25;
+    };
 
-    #   programs.java = {
-    #     enable = true;
-    #     package = pkgs.zulu25;
-    #   };
-
-    #   home.packages = with pkgs; [
-    #     gradle_9
-    #   ];
-    # };
+    environment.systemPackages = with pkgs; [
+      gradle_9
+    ];
   };
 }
