8aa8f6eaaed430a0592114fa0a99f6bb3d2f2b9d
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-03-28 09:15:12 +0100

    refactor: simplify function signature in noctalia module

--- modules/features/noctalia/noctalia.nix
+++ modules/features/noctalia/noctalia.nix
@@ -1,8 +1,4 @@
-{
-  self,
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   perSystem = {pkgs, ...}: {
     packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
