b97d45043133410de020df6e3ee8ec70a7b383c0
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-03-31 10:10:47 +0200

    fix: update Spotify hotkey command and improve parts.nix formatting

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,3 +1,13 @@
-{inputs, ...}: {
-  imports = [inputs.flake-parts.flakeModules.modules];
+{ inputs, ... }:
+{
+  imports = [ inputs.flake-parts.flakeModules.modules ];
+
+  perSystem =
+    { system, ... }:
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
 }
