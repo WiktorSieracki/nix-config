e9a63812e914287c81d9a8accef440c290b0d7a7
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-06 13:49:19 +0200

    feat: add wallpaper overview

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -2,125 +2,138 @@
   self,
   inputs,
   ...
-}: {
-  flake.modules.nixos.niri = {pkgs, ...}: {
-    programs.niri = {
-      enable = true;
-      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+}:
+{
+  flake.modules.nixos.niri =
+    { pkgs, ... }:
+    {
+      programs.niri = {
+        enable = true;
+        package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+      };
     };
-  };
 
-  perSystem = {
-    pkgs,
-    lib,
-    self',
-    ...
-  }: {
-    packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
-      inherit pkgs;
-      settings = {
-        spawn-at-startup = [
-          (lib.getExe self'.packages.myNoctalia)
-        ];
-        prefer-no-csd = null;
-        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
-        input.keyboard.xkb.layout = "pl";
-        layout.gaps = 5;
-        layout.background-color = "transparent";
-
-        outputs."HP Inc. HP E243 CNC0171FR8" = {
-          mode = "1920x1080@60.000";
-          "position x=0 y=0" = null;
-        };
-        outputs."Ancor Communications Inc ASUS VX239 G6LMTJ040329" = {
-          mode = "1920x1080@60.000";
-          "position x=1920 y=0" = null;
-        };
-
-        window-rules = [
-          {
-            matches = [{app-id = "code";}];
-            default-column-width = {
-              proportion = 1.0;
-            };
-          }
-        ];
-
-        binds = {
-          "Mod+C hotkey-overlay-title=\"Open nix-config in an editor\"".spawn-sh = "code ~/.config/nix-config";
-          "Mod+A hotkey-overlay-title=\"Open gemini in Firefox\"".spawn = [
-            "${lib.getExe pkgs.firefox}"
-            "--new-window"
-            "https://gemini.google.com"
+  perSystem =
+    {
+      pkgs,
+      lib,
+      self',
+      ...
+    }:
+    {
+      packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
+        inherit pkgs;
+        settings = {
+          spawn-at-startup = [
+            (lib.getExe self'.packages.myNoctalia)
           ];
-          "Mod+N hotkey-overlay-title=\"Open notion in Firefox\"".spawn = [
-            "${lib.getExe pkgs.firefox}"
-            "--new-window"
-            "https://www.notion.so"
+          prefer-no-csd = null;
+          xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+          input.keyboard.xkb.layout = "pl";
+          layout.gaps = 5;
+          layout.background-color = "transparent";
+
+          outputs."HP Inc. HP E243 CNC0171FR8" = {
+            mode = "1920x1080@60.000";
+            "position x=0 y=0" = null;
+          };
+          outputs."Ancor Communications Inc ASUS VX239 G6LMTJ040329" = {
+            mode = "1920x1080@60.000";
+            "position x=1920 y=0" = null;
+          };
+
+          window-rules = [
+            {
+              matches = [ { app-id = "code"; } ];
+              default-column-width = {
+                proportion = 1.0;
+              };
+            }
           ];
-          "Mod+S hotkey-overlay-title=\"Open Spotify\"".spawn = ["${lib.getExe pkgs.spotify}"];
-          "Mod+D hotkey-overlay-title=\"Open Discord\"".spawn = ["${lib.getExe pkgs.discord}"];
-          "Mod+B hotkey-overlay-title=\"Open a Browser: Firefox\"".spawn = "${lib.getExe pkgs.firefox}";
 
-          "Mod+Return".spawn-sh = lib.getExe pkgs.alacritty;
-          "Mod+W repeat=false".close-window = null;
-          "Mod+Space".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
-          "Mod+F".maximize-column = null;
-          "Mod+O repeat=false".toggle-overview = null;
-          "Mod+P".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call sessionMenu toggle";
+          layer-rules = [
+            {
+              matches = [ { namespace = "^noctalia-wallpaper"; } ];
+              place-within-backdrop = true;
+            }
+          ];
 
-          "Mod+Shift+Slash".show-hotkey-overlay = null;
+          binds = {
+            "Mod+C hotkey-overlay-title=\"Open nix-config in an editor\"".spawn-sh =
+              "code ~/.config/nix-config";
+            "Mod+A hotkey-overlay-title=\"Open gemini in Firefox\"".spawn = [
+              "${lib.getExe pkgs.firefox}"
+              "--new-window"
+              "https://gemini.google.com"
+            ];
+            "Mod+N hotkey-overlay-title=\"Open notion in Firefox\"".spawn = [
+              "${lib.getExe pkgs.firefox}"
+              "--new-window"
+              "https://www.notion.so"
+            ];
+            "Mod+S hotkey-overlay-title=\"Open Spotify\"".spawn = [ "${lib.getExe pkgs.spotify}" ];
+            "Mod+D hotkey-overlay-title=\"Open Discord\"".spawn = [ "${lib.getExe pkgs.discord}" ];
+            "Mod+B hotkey-overlay-title=\"Open a Browser: Firefox\"".spawn = "${lib.getExe pkgs.firefox}";
 
-          "Mod+H".focus-column-left = null;
-          "Mod+L".focus-column-right = null;
-          "Mod+K".focus-window-up = null;
-          "Mod+J".focus-window-down = null;
+            "Mod+Return".spawn-sh = lib.getExe pkgs.alacritty;
+            "Mod+W repeat=false".close-window = null;
+            "Mod+Space".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+            "Mod+F".maximize-column = null;
+            "Mod+O repeat=false".toggle-overview = null;
+            "Mod+P".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call sessionMenu toggle";
 
-          "Mod+Left".focus-column-left = null;
-          "Mod+Right".focus-column-right = null;
-          "Mod+Up".focus-window-up = null;
-          "Mod+Down".focus-window-down = null;
+            "Mod+Shift+Slash".show-hotkey-overlay = null;
 
-          "Mod+Shift+H".move-column-left = null;
-          "Mod+Shift+L".move-column-right = null;
-          "Mod+Shift+K".move-window-up = null;
-          "Mod+Shift+J".move-window-down = null;
+            "Mod+H".focus-column-left = null;
+            "Mod+L".focus-column-right = null;
+            "Mod+K".focus-window-up = null;
+            "Mod+J".focus-window-down = null;
 
-          "Mod+1".focus-workspace = 1;
-          "Mod+2".focus-workspace = 2;
-          "Mod+3".focus-workspace = 3;
-          "Mod+4".focus-workspace = 4;
-          "Mod+5".focus-workspace = 5;
-          "Mod+6".focus-workspace = 6;
-          "Mod+7".focus-workspace = 7;
-          "Mod+8".focus-workspace = 8;
-          "Mod+9".focus-workspace = 9;
+            "Mod+Left".focus-column-left = null;
+            "Mod+Right".focus-column-right = null;
+            "Mod+Up".focus-window-up = null;
+            "Mod+Down".focus-window-down = null;
 
-          "Mod+Shift+1".move-column-to-workspace = 1;
-          "Mod+Shift+2".move-column-to-workspace = 2;
-          "Mod+Shift+3".move-column-to-workspace = 3;
-          "Mod+Shift+4".move-column-to-workspace = 4;
-          "Mod+Shift+5".move-column-to-workspace = 5;
-          "Mod+Shift+6".move-column-to-workspace = 6;
-          "Mod+Shift+7".move-column-to-workspace = 7;
-          "Mod+Shift+8".move-column-to-workspace = 8;
-          "Mod+Shift+9".move-column-to-workspace = 9;
+            "Mod+Shift+H".move-column-left = null;
+            "Mod+Shift+L".move-column-right = null;
+            "Mod+Shift+K".move-window-up = null;
+            "Mod+Shift+J".move-window-down = null;
 
-          "Mod+WheelScrollDown cooldown-ms=150".focus-workspace-down = null;
-          "Mod+WheelScrollUp cooldown-ms=150".focus-workspace-up = null;
-          "Mod+WheelScrollRight".focus-column-right = null;
-          "Mod+WheelScrollLeft".focus-column-left = null;
+            "Mod+1".focus-workspace = 1;
+            "Mod+2".focus-workspace = 2;
+            "Mod+3".focus-workspace = 3;
+            "Mod+4".focus-workspace = 4;
+            "Mod+5".focus-workspace = 5;
+            "Mod+6".focus-workspace = 6;
+            "Mod+7".focus-workspace = 7;
+            "Mod+8".focus-workspace = 8;
+            "Mod+9".focus-workspace = 9;
 
-          "Mod+Shift+WheelScrollDown cooldown-ms=150".move-column-to-workspace-down = null;
-          "Mod+Shift+WheelScrollUp cooldown-ms=150".move-column-to-workspace-up = null;
-          "Mod+Shift+WheelScrollRight".move-column-right = null;
-          "Mod+Shift+WheelScrollLeft".move-column-left = null;
+            "Mod+Shift+1".move-column-to-workspace = 1;
+            "Mod+Shift+2".move-column-to-workspace = 2;
+            "Mod+Shift+3".move-column-to-workspace = 3;
+            "Mod+Shift+4".move-column-to-workspace = 4;
+            "Mod+Shift+5".move-column-to-workspace = 5;
+            "Mod+Shift+6".move-column-to-workspace = 6;
+            "Mod+Shift+7".move-column-to-workspace = 7;
+            "Mod+Shift+8".move-column-to-workspace = 8;
+            "Mod+Shift+9".move-column-to-workspace = 9;
 
-          "Mod+Shift+E".quit = null;
-          "Ctrl+Alt+Delete".quit = null;
-          "Print".screenshot = null;
+            "Mod+WheelScrollDown cooldown-ms=150".focus-workspace-down = null;
+            "Mod+WheelScrollUp cooldown-ms=150".focus-workspace-up = null;
+            "Mod+WheelScrollRight".focus-column-right = null;
+            "Mod+WheelScrollLeft".focus-column-left = null;
+
+            "Mod+Shift+WheelScrollDown cooldown-ms=150".move-column-to-workspace-down = null;
+            "Mod+Shift+WheelScrollUp cooldown-ms=150".move-column-to-workspace-up = null;
+            "Mod+Shift+WheelScrollRight".move-column-right = null;
+            "Mod+Shift+WheelScrollLeft".move-column-left = null;
+
+            "Mod+Shift+E".quit = null;
+            "Ctrl+Alt+Delete".quit = null;
+            "Print".screenshot = null;
+          };
         };
       };
     };
-  };
 }
