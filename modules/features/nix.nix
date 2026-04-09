7abf4d4a09b9adf26f84b3e302ad43bed6044143
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-09 15:51:34 +0200

    feat: format nix settings for improved readability and organization

--- modules/features/nix.nix
+++ modules/features/nix.nix
@@ -1,7 +1,18 @@
 {
   flake.modules.nixos.nix = {pkgs, ...}: {
     nixpkgs.config.allowUnfree = true;
-    nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    nix.settings.substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    nix.settings.trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:3B5GJAjJHHfXBaDt4svcgOdgQ1w7kFZ3b6sDZpQdExI="
+    ];
 
     environment.systemPackages = with pkgs; [
       alejandra
