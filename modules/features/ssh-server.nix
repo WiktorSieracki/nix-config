34cceca9bd6166f35f384853f8753447d928d5f0
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-11 00:32:57 +0200

    feat: add SSH server configuration and update SSH settings for multiple devices

---
+++ modules/features/ssh-server.nix
@@ -0,0 +1,13 @@
+{
+  flake.modules.nixos.ssh-server = {
+    services.openssh = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+  };
+}
