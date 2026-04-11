ea3db97024ff27f10f41790955ee3378b9dbc3f7
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-12 00:34:56 +0200

    feat: enable nix-ld program in base configuration

--- modules/features/base.nix
+++ modules/features/base.nix
@@ -4,6 +4,7 @@
     hardware.bluetooth.enable = true;
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
+    programs.nix-ld.enable = true;
 
     services.xserver.enable = true;
     services.displayManager.gdm.enable = true;
