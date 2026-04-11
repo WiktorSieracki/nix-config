01e074694b8f47da0085549b2df3eacff153f86b
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-11 20:45:42 +0200

    feat: add authorized keys for SSH user configuration

--- modules/features/ssh-server.nix
+++ modules/features/ssh-server.nix
@@ -1,5 +1,11 @@
 {
   flake.modules.nixos.ssh-server = {
+    users.users.wiktor = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDwctBSDTMy2mf8LC0WKXnEbYl5mlBLGmtmEJNBpNXR"
+      ];
+    };
+
     services.openssh = {
       enable = true;
       openFirewall = true;
