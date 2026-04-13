04db7887df74a1654492f1a7a3cc1c77132c8e6b
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-14 00:08:13 +0200

    refactor: improve formatting and consistency in base Nix configuration

--- modules/features/base.nix
+++ modules/features/base.nix
@@ -1,30 +1,29 @@
 {
-  flake.modules.nixos.nixos =
-    { pkgs, ... }:
-    {
-      networking.networkmanager.enable = true;
-      hardware.bluetooth.enable = true;
-      services.power-profiles-daemon.enable = true;
-      services.upower.enable = true;
-      programs.nix-ld.enable = true;
+  flake.modules.nixos.nixos = {pkgs, ...}: {
+    networking.networkmanager.enable = true;
+    hardware.bluetooth.enable = true;
+    services.power-profiles-daemon.enable = true;
+    services.upower.enable = true;
+    programs.nix-ld.enable = true;
 
-      services.xserver.enable = true;
-      services.displayManager.gdm.enable = true;
+    services.xserver.enable = true;
+    services.displayManager.gdm.enable = true;
 
-      environment.systemPackages = with pkgs; [
-        tree
-        treecat
-        tealdeer
-        bruno-cli
-        neovim
-        nautilus
-        libreoffice-fresh
-        qalculate-gtk
-        evince
-        file-roller
-        p7zip
-        vlc
-        gnome-disk-utility
-      ];
-    };
+    environment.systemPackages = with pkgs; [
+      tree
+      treecat
+      tealdeer
+      bruno-cli
+      neovim
+      nautilus
+      libreoffice-fresh
+      qalculate-gtk
+      evince
+      file-roller
+      p7zip
+      vlc
+      gnome-disk-utility
+      pinta
+    ];
+  };
 }
