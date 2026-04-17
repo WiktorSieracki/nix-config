74db8cdcf1120127c2c2410c8d23358e64ed5a42
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-17 21:53:58 +0200

    feat: enhance git and sops configurations, add studentEmail secret

--- modules/features/programming/git.nix
+++ modules/features/programming/git.nix
@@ -1,12 +1,27 @@
-{
+{inputs, ...}: {
   flake.modules = {
-    homeManager.git = {
+    homeManager.git = {config, ...}: {
+      imports = [
+        inputs.sops-nix.homeManagerModules.sops
+      ];
+
+      sops.secrets.studentEmail = {};
+
+      sops.templates."git-user-email".content = ''
+        [user]
+          email = ${config.sops.placeholder.studentEmail}
+      '';
+
       programs.git = {
         enable = true;
         signing.format = null;
+        includes = [
+          {
+            path = config.sops.templates."git-user-email".path;
+          }
+        ];
 
         settings.user = {
-          email = "w.sieracki.643@studms.ug.edu.pl";
           name = "Wiktor Sieracki";
         };
       };
--- modules/features/sops.nix
+++ modules/features/sops.nix
@@ -1,29 +1,47 @@
 {inputs, ...}: {
-  flake.modules.nixos.sops = {pkgs, ...}: {
-    imports = [
-      inputs.sops-nix.nixosModules.sops
-    ];
+  flake.modules = {
+    nixos.sops = {pkgs, ...}: {
+      imports = [
+        inputs.sops-nix.nixosModules.sops
+      ];
 
-    environment.systemPackages = with pkgs; [
-      sops
-    ];
+      environment.systemPackages = with pkgs; [
+        sops
+      ];
 
-    sops = {
-      # The age key file used to decrypt secrets (your user key for SOPS CLI)
-      age.sshKeyPaths = ["/home/wiktor/.ssh/id_ed25519"];
+      sops = {
+        # The age key file used to decrypt secrets (your user key for SOPS CLI)
+        age.sshKeyPaths = ["/home/wiktor/.ssh/id_ed25519"];
 
-      # The default sops file to use for secrets
-      defaultSopsFile = ../../secrets.yaml;
-      validateSopsFiles = false;
+        # The default sops file to use for secrets
+        defaultSopsFile = ../../secrets.yaml;
+        validateSopsFiles = false;
 
-      secrets = {
-        # This decrypts the "eduroamPassword" key from secrets.yaml
+        secrets = {
+          eduroamPassword = {};
+          studentEmail = {};
+        };
+
+        # someOption = config.sops.secrets.hello.path;
+        # if home-manager switch fails because of sops try running:
+        # systemctl --user reset-failed
+      };
+    };
+    homeManager.sops = {
+      imports = [
+        inputs.sops-nix.homeManagerModules.sops
+      ];
+
+      sops.secrets = {
         eduroamPassword = {};
+        studentEmail = {};
       };
 
-      # someOption = config.sops.secrets.hello.path;
-      # if home-manager switch fails because of sops try running:
-      # systemctl --user reset-failed
+      sops = {
+        age.sshKeyPaths = ["/home/wiktor/.ssh/id_ed25519"];
+        defaultSopsFile = ../../secrets.yaml;
+        validateSopsFiles = false;
+      };
     };
   };
 }
