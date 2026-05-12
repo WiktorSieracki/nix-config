d9faf59d669cbafc18a2c3a3137b6d235d4cd347
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-05-12 20:33:07 +0200

    fix: correct LinkedIn URL in personal snippets configuration

--- modules/features/personal-snippets.nix
+++ modules/features/personal-snippets.nix
@@ -3,6 +3,6 @@
     "Mod+Ctrl+N".spawn-sh = "${pkgs.wtype}/bin/wtype 'Wiktor Sieracki'";
     "Mod+Ctrl+E".spawn-sh = "${pkgs.wtype}/bin/wtype \"$(cat /run/secrets/personalEmail)\"";
     "Mod+Ctrl+G".spawn-sh = "${pkgs.wtype}/bin/wtype 'https://github.com/WiktorSieracki'";
-    "Mod+Ctrl+I".spawn-sh = "${pkgs.wtype}/bin/wtype 'https://www.linkedin.com/in/wiktor-sieracki-b54817272/'";
+    "Mod+Ctrl+I".spawn-sh = "${pkgs.wtype}/bin/wtype 'https://www.linkedin.com/in/wiktor-sieracki/'";
   };
 }
