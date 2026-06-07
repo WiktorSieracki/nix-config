e87165aeb96034b9e660152ef777c794b4934d60
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-06-07 11:12:39 +0200

    feat: add firewall configuration for localsend application

--- modules/features/apps/localsend.nix
+++ modules/features/apps/localsend.nix
@@ -3,5 +3,9 @@
     environment.systemPackages = with pkgs; [
       localsend
     ];
+    networking.firewall = {
+      allowedTCPPorts = [53317];
+      allowedUDPPorts = [53317];
+    };
   };
 }
